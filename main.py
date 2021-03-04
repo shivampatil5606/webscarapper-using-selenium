c7e3441a4ba494d363054d3da0bfe7bc519faf1a
Shivam Patil <54853569+shivampatil5606@users.noreply.github.com>
2021-03-04 21:13:10 +0530

    main file

---
+++ main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+
+
+def main(headless=True):
+    # Give the exact path of the chromedriver.exe
+    if headless:
+        driver = webdriver.Chrome('chromedriver.exe', options=options)
+    else:
+        driver = webdriver.Chrome('chromedriver.exe')
+    url = "Enter URL to search" # Enter correct url
+    driver.get(url)
+    print('Message to print(Succesful)')
+    
+    # If you are using doing some kind of login then you can use the following code.
+    username = driver.find_element_by_id("username")  # Your id may be different. Inspect the page to get the id.
+    password = driver.find_element_by_id("password")
+    USERNAME = "your username to enter"
+    PASSWORD = "Your password"
+    username.send_keys(USERNAME)
+    password.send_keys(PASSWORD)
+    # You can also use the click() function to click on some element
+    driver.find_element_by_id("loginbtn").click()
+    print("Logged IN..")
+    
+    driver.quit() # To exit and close chrome.
+   
+        
+        
+        
+main()  # Specify flase to open chrome
